074d0409c768a73356ebd520bf0c3406508e3dd8
moonyoungCHAE <xpf_fl@naver.com>
2025-12-30 19:26:07 +0900

    ack test .
    Signed-off-by: moonyougnCHAE <xpf_fl@naver.com>

--- spring-kafka/src/test/java/org/springframework/kafka/listener/DefaultErrorHandlerAckAfterCommitExceptionMonoListenerTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/listener/DefaultErrorHandlerAckAfterCommitExceptionMonoListenerTests.java
@@ -1,64 +1,103 @@
+/*
+ * Copyright 2022-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.kafka.listener;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.apache.kafka.common.record.TimestampType;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ContainerProperties.AckMode;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.support.LogIfLevelEnabled;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.util.backoff.FixedBackOff;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
+import java.time.Duration;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willAnswer;
+import static org.mockito.Mockito.*;
 
-
+/**
+ * @author Gary Russell
+ * @since 2.9
+ *
+ */
 @SpringJUnitConfig
 @DirtiesContext
-@EmbeddedKafka(topics = "dehm")
-public class DefaultErrorHandlerSeekAfterCommitExceptionMonoListenerTest {
+public class DefaultErrorHandlerAckAfterCommitExceptionMonoListenerTests {
+
 	@Test
-	void testAckAfterHandlerAsync(@Autowired Config config,
+	void testAckAfterHandlerAsync(@Autowired DefaultErrorHandlerAckAfterCommitExceptionMonoListenerTests.Config config,
 								  @Autowired KafkaTemplate<Integer, String> template)
 			throws InterruptedException {
 
-		for (int i = 0; i < 6; i++) {
-			template.send("dehm", 0, null, "message contents");
+		for (int i = 0; i < 3; i++) {
+			template.send("dehm2", 0, null, "message contents");
 		}
-		assertThat(config.latch.await(10, TimeUnit.SECONDS))
-				.describedAs("CountDownLatch.count=%d", config.latch.getCount())
-				.isTrue();
+//		assertThat(config.latch.await(10, TimeUnit.SECONDS))
+//				.describedAs("CountDownLatch.count=%d", config.latch.getCount())
+//				.isTrue();
 
-		for (int i = 0; i < 6; i++) {
-			assertThat(config.offsetCountMap.containsKey(i)).isTrue();
-			if (i == 1) {
-				assertThat(config.offsetCountMap.get(1)).isEqualTo(2);
-			} else {
-				assertThat(config.offsetCountMap.get(i)).isEqualTo(1);
-			}
-		}
+		Thread.sleep(1000);
+
+		verify(this.consumer).commitSync(
+				Collections.singletonMap(new TopicPartition("dehm2", 0), new OffsetAndMetadata(1L)),
+				Duration.ofSeconds(60));
+
+//		for (int i = 0; i < 6; i++) {
+//			assertThat(config.offsetCountMap.containsKey(i)).isTrue();
+//			if (i == 1) {
+//				assertThat(config.offsetCountMap.get(1)).isEqualTo(2);
+//			} else {
+//				assertThat(config.offsetCountMap.get(i)).isEqualTo(1);
+//			}
+//		}
 
 	}
 
 	@Configuration
 	@EnableKafka
 	public static class Config {
-		final CountDownLatch latch = new CountDownLatch(7);
+		private final CountDownLatch latch = new CountDownLatch(4);
 		private final Map<Integer, Integer> offsetCountMap = new ConcurrentHashMap<>();
 
 		@KafkaListener(id = "dehm.id", topics = "dehm")
@@ -69,9 +108,6 @@ public class DefaultErrorHandlerSeekAfterCommitExceptionMonoListenerTest {
 			this.latch.countDown();
 
 			if (record.offset() == 1) {
-				if (count == 2) {
-					return  Mono.empty();
-				}
 				throw new RuntimeException("Exception for error handler");
 			}
 			return Mono.empty();
@@ -91,7 +127,7 @@ public class DefaultErrorHandlerSeekAfterCommitExceptionMonoListenerTest {
 
 		@Bean
 		ConsumerFactory<Integer, String> consumerFactory(EmbeddedKafkaBroker broker) {
-			Map<String, Object> props = KafkaTestUtils.consumerProps("dehm.grp", "false", broker);
+			Map<String, Object> props = KafkaTestUtils.consumerProps("dehm2.grp", "false", broker);
 			props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 3);
 			return new DefaultKafkaConsumerFactory<>(
 					props);
@@ -109,4 +145,5 @@ public class DefaultErrorHandlerSeekAfterCommitExceptionMonoListenerTest {
 			return new KafkaTemplate<>(pf);
 		}
 	}
+
 }
